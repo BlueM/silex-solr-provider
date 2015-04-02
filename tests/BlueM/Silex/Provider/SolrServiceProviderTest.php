2bf79d92465a7ee0617b78432a420f8ee375d0b0
Carsten Blüm <carsten@bluem.net>
2015-04-02 12:02:42 +0200

    Add tests

---
+++ tests/BlueM/Silex/Provider/SolrServiceProviderTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace BlueM\Silex\Provider;
+
+require_once __DIR__.'/../../../../../../../vendor/autoload.php';
+require_once __DIR__.'/../../../../src/BlueM/Silex/Provider/SolrServiceProvider.php';
+
+/**
+ * Unit tests for BlueM\Silex\Provider\SolrServiceProvider
+ *
+ * @covers BlueM\Silex\Provider\SolrServiceProvider
+ */
+class SolrServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @test
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage non-empty string
+	 */
+	public function thePrefixMustNotBeEmpty()
+	{
+        new SolrServiceProvider('');
+	}
+
+	/**
+	 * @test
+	 */
+	public function thePrefixPassedAsArgumentIsSaved()
+	{
+        $subject = new SolrServiceProvider('myprefix');
+
+        $prefixProperty = new \ReflectionProperty($subject, 'prefix');
+        $prefixProperty->setAccessible(true);
+
+        $this->assertSame('myprefix', $prefixProperty->getValue($subject));
+	}
+
+    /**
+     * @test
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage No Solr options are defined
+     */
+    public function anExceptionIsThrownIfNoOptionsAreDefined()
+    {
+        $subject = new SolrServiceProvider();
+        $method = new \ReflectionMethod($subject, 'getOptions');
+        $method->setAccessible(true);
+
+        $appStub = $this->getMockBuilder('Silex\Application')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $appStub->expects($this->exactly(17))
+            ->method('offsetExists')
+            ->will($this->returnValue(false));
+
+        $method->invoke($subject, $appStub);
+    }
+
+    /**
+     * @test
+     */
+    public function getTheOptions()
+    {
+        $subject = new SolrServiceProvider();
+        $method = new \ReflectionMethod($subject, 'getOptions');
+        $method->setAccessible(true);
+
+        $appStub = $this->getMockBuilder('Silex\Application')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $valueMap = [
+            ['solr.port', '1.2.3.4'],
+            ['solr.timeout', 4711],
+        ];
+
+        $appStub->expects($this->exactly(17))
+            ->method('offsetExists')
+            ->will($this->returnValueMap($valueMap));
+
+        $appStub->expects($this->exactly(count($valueMap)))
+            ->method('offsetGet')
+            ->will($this->returnValueMap($valueMap));
+
+        $options = $method->invoke($subject, $appStub);
+
+        $this->assertSame(['port' => '1.2.3.4', 'timeout' => 4711], $options);
+    }
+}
